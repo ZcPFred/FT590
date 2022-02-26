05ed612ac716e69cde0ed2f20309da744008b24f
ZcPFred <92492287+ZcPFred@users.noreply.github.com>
2022-02-26 08:38:40 -0500

    Delete ES.py

--- Week05/ES.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def ES(ret, alpha):
-    VaR_t = -np.percentile(ret, alpha*100)
-    ret_alphatail= ret[ret < -VaR_t]
-    ES = ret_alphatail.mean()
-    return -ES
